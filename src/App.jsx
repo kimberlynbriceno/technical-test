42dc63bdf8c5ebf1ecd4f4789e9b04c65dd9ab1b
kimberlyn <kimberlynbriceno22@gmail.com>
2023-03-15 15:54:14 -0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1 className='font-bold text-white'>prueba tecnica</h1>
+    </div>
+  )
+}
+
+export default App
